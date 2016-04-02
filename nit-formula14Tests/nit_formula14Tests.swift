0935365e0c6f71f9f5ff98279fe544ce50ff9fcd
Takarkiz <ponpontanuki16@gmail.com>
2016-04-02 15:50:29 +0900

    Initial Commit

---
+++ nit-formula14Tests/nit_formula14Tests.swift
@@ -0,0 +1,36 @@
+//
+//  nit_formula14Tests.swift
+//  nit-formula14Tests
+//
+//  Created by 澤田昂明 on 2016/04/02.
+//  Copyright © 2016年 Takarki. All rights reserved.
+//
+
+import XCTest
+@testable import nit_formula14
+
+class nit_formula14Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
